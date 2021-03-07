71306b5d6224d3ed50a7e4720532178c62ea43d3
= <=>
2021-03-07 17:28:15 +0100

    Template Structure Arduino
    Implemented a switch handling incoming requests as specified in IO_hub_interface.txt. Commands can be issued over serial monitor.

--- Arduino_code/toString/toString.ino
+++ Arduino_code/toString/toString.ino
@@ -1,36 +1,120 @@
-//define a timer
-//define a standard response
-unsigned long timer = 0;
-int connecter = 1;
+//====COMMAND NUMBERS====
+//Gate control:
+const int CLOSE_GATE = 10;
+const int OPEN_GATE = 50;
+const int DO_PUSH = 30;
+//Sensor request:
+const int GET_COLOR = 20;
+const int AFFIRM_DISK = 40;
+//Motor control:
+const int STRING_DISK = 60;
+//Program state control:
+const int PING = 100;
 
+//====RESPONSE NUMBERS====
+//Action control:
+const int NOTIFY_DISK_PRESENCE = 1;
+const int CONFIRM_CLOSE_GATE = 11;
+const int CONFIRM_OPEN_GATE = 51;
+const int CONFIRM_DO_PUSH = 31;
+const int CONFIRM_STRING_DISK = 61;
+//Program state messages:
+const int UNEXPECTED_ERROR = -1;
+const int PONG = 101;
+//For sensor requests, the resulting value functions as response message.
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("Successfully started IO hub");
 }
 
 //Communicate to the RPI that we're still operational.
-void check(){
-  if (Serial.available() > 0) {
-    char message = Serial.read();
-    switch (message){
-      case '4':
-        Serial.write(23);
-        // do some action
-        delay(100);
-        break;
-      default:
-        // we get something we did not understand
-        // we should normally send an error message but for now we just ignore it 
-        Serial.println("unknown action!");
-        break;
-    }
+void check(int issuedCommand) {
+  switch (issuedCommand) {
+    case PING:
+      Serial.println(PONG);
+      // do some action
+      delay(100);
+      break;
+      
+    case CLOSE_GATE:
+      Serial.println(CONFIRM_CLOSE_GATE);
+      // do some action
+      delay(100);
+      break;
+
+    case OPEN_GATE:
+      Serial.println(CONFIRM_OPEN_GATE);
+      // do some action
+      delay(100);
+      break;
+
+    case DO_PUSH:
+      Serial.println(CONFIRM_DO_PUSH);
+      // do some action
+      delay(100);
+      break;
+
+    case GET_COLOR:
+      Serial.println("SOME VALUE");
+      // do some action
+      delay(100);
+      break;
+
+    case AFFIRM_DISK:
+      Serial.println("I THINK, I YES");
+      // do some action
+      delay(100);
+      break;
+
+    case STRING_DISK:
+      Serial.println(CONFIRM_STRING_DISK);
+      // do some action
+      delay(100);
+      break;
+
+    default:
+      // we get something we did not understand
+      // we should normally send an error message but for now we just ignore it
+      Serial.print("unknown action! ");
+      Serial.println(UNEXPECTED_ERROR);
+      break;
   }
 }
 
+int value = 0;
+
 void loop() {
-  if(millis() - 1000 > timer){
-    timer = millis();
-    Serial.write(connecter);
+
+//This if-statement is responsible for taking input
+//from the serial monitor. The call to check() is what truly
+//handles the command.
+
+//Check if there is a command waiting for me
+  if (Serial.available() > 0) {
+    char nextChar = Serial.read();
+    //Have I finished reading this command?
+    if (nextChar == '\n') {
+      Serial.print("SENT: ");
+      Serial.println(value);
+      Serial.print("RECEIVED: ");
+      //Send the command
+      check(value);
+      value = 0;
+      //Otherwise, continue constructing the command issued
+    } else {
+      int intValue = nextChar - '0';
+      value = 10 * value;
+      value = value + intValue;
+    }
   }
-  check();
+
+
+
+
+  // TODO: Implement scanning to write to "diskFound"
+  bool diskFound = false;
+  if (diskFound) {
+    Serial.write(NOTIFY_DISK_PRESENCE);
+  }
 }
