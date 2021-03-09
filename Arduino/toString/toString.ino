70eddb929c4b49d8ca06f38de3f05f00561359c9
JeroenLamme <64089815+JeroenLamme@users.noreply.github.com>
2021-03-09 17:22:51 +0100

    Implemented state checking for messages

--- Arduino/toString/toString.ino
+++ Arduino/toString/toString.ino
@@ -11,12 +11,19 @@ const int STRING_DISK = 60;
 //Program state control:
 const int PING = 100;
 const int SET_ERROR_STATE = 102;
-const int EXIT_ERRO_STATE = 104;
+const int EXIT_ERROR_STATE = 104;
 const int GET_ERROR_STATE_INFO = 106;
 //Callibration control:
 const int SET_WHITE = 200;
 const int SET_BLACK = 202;
-const int EXIT_SETUP = 204
+const int EXIT_SETUP = 204;
+
+//working state possibilities
+int workmessages[] = {CLOSE_BLOCKER, OPEN_BLOCKER, DO_PUSH, GET_COLOR, AFFIRM_DISK, STRING_DISK, PING, SET_ERROR_STATE};
+int errormessages[] = {EXIT_ERROR_STATE, GET_ERROR_STATE_INFO, PING};
+int setupmessages[] = {SET_WHITE, SET_BLACK, EXIT_SETUP, PING};
+int all[] = {CLOSE_BLOCKER, OPEN_BLOCKER, DO_PUSH, GET_COLOR, AFFIRM_DISK, STRING_DISK,
+            PING, SET_ERROR_STATE,EXIT_ERROR_STATE, GET_ERROR_STATE_INFO, SET_WHITE, SET_BLACK, EXIT_SETUP};
 
 //====RESPONSE NUMBERS====
 //Action control:
@@ -32,7 +39,6 @@ const int UNKNOWN_COMMAND = -3;
 const int PONG = 101;
 const int ERRONG_PING = 103;
 const int CONFIRM_EXIT_ERROR_STATE = 105;
-const int GET_ERROR_STATE_INFO = 106;
 const int START_MESSAGE = 107;
 const int END_MESSGE = 108;
 const int CONFIRM_SET_WHITE = 201;
@@ -42,17 +48,17 @@ const int CONFIRM_EXIT_SETUP = 205;
 const int WHITE = 21;
 const int BLACK = 22;
 const int NEITHER = 23;
+// A timer to check if we are not disconnected
+unsigned long timer = 0;
+/* The current state we are in:
+ * 0: the initialisation state 
+ * 1: the working state
+ * 2: the error state
+ */
+int state = 0;
 
 void setup() {
   Serial.begin(9600);
-  // A timer to check if we are not disconnected
-  unsigned long timer = 0;
-  /* The current state we are in:
-  * 0: the initialisation state 
-  * 1: the working state
-  * 2: the error state
-  */
-  int state = 0;
   //Serial.println("Successfully started IO hub");
 }
 
@@ -60,7 +66,7 @@ void setup() {
 void check(int issuedCommand) {
   switch (issuedCommand) {
     case PING:
-      Serial.write(PONG)
+      Serial.write(PONG);
       break;
 
     case SET_WHITE:
@@ -75,13 +81,13 @@ void check(int issuedCommand) {
       delay(100);
       break;
 
-    case CLOSE_GATE:
+    case CLOSE_BLOCKER:
       Serial.println(CONFIRM_CLOSE_GATE);
       // do some action
       delay(100);
       break;
 
-    case OPEN_GATE:
+    case OPEN_BLOCKER:
       Serial.println(CONFIRM_OPEN_GATE);
       // do some action
       delay(100);
@@ -114,9 +120,52 @@ void check(int issuedCommand) {
     default:
       // we get something we did not understand
       // we should normally send an error message but for now we just ignore it
-      Serial.print("unknown action! ");
-      Serial.println(UNEXPECTED_ERROR);
+    //  Serial.print("unknown action! ");
+     // Serial.println(UNEXPECTED_ERROR);
       break;
+  }
+}
+
+boolean in(int number, int commands[]){
+  for(int i = 0; i < sizeof(commands) / sizeof(commands[0]); i++){
+    if(number == commands[i]){
+      return true;
+    }
+  }
+  return false;    
+}
+
+boolean statecheck(int message) {
+  if(state == 0){
+    if(in(message,setupmessages)){
+      return true;
+    }else if(in(message,all)){
+      Serial.write(ILLEGAL_COMMAND);
+      return false;
+    }else{
+      Serial.write(UNKNOWN_COMMAND);
+      return false;
+    }
+  }else if(state == 1){
+    if(in(message,workmessages)){
+      return true;
+    }else if(in(message,all)){
+      Serial.write(ILLEGAL_COMMAND);
+      return false;
+    }else{
+      Serial.write(UNKNOWN_COMMAND);
+      return false;
+    }
+  }else if(state == 2){
+    if(in(message,errormessages)){
+      return true;
+    }else if(in(message,all)){
+      Serial.write(ILLEGAL_COMMAND);
+      return false;
+    }else{
+      Serial.write(UNKNOWN_COMMAND);
+      return false;
+    }
   }
 }
 
@@ -131,7 +180,10 @@ void loop() {
 //Check if there is a command waiting for me
   if (Serial.available() > 0) {
     int message = Serial.read() - '0';
-    check(value);
+    //first we need to check if the command is valid in the current state if so check it
+    if(statecheck(message)){
+      check(message);
+    }
   }
 
 
