7d00359f4b8c322933256095b841abacae72ef87
Adrian <a.a.t.beers@student.tue.nl>
2021-03-10 23:05:33 +0100

    Specify and implement Processor, ExpectationHandler, and Expectation
    Specifies and implements Processor, adds, specifies, and implements ExpectationHandler and Expectation.

--- RPI/src/processor.py
+++ RPI/src/processor.py
@@ -1,14 +1,40 @@
 class Processor:
-    """Processes received messages for the communicator."""
+    """
+    Processes input and returns output for the communicator.
 
-    __protocol_handler = None
-    """Protocol handler to communicate with protocol."""
+    Attributes
+    __________
+    __protocol_handler : ProtocolHandler
+        An instance of the protocol handler to communicate with protocol
 
-    __stringer = None
-    """Stringer to track string status."""
+    __expectation_handler : ExpectationHandler
+        An instance of the expectation handler to track expected inputs
 
-    def __init__(self, protocol_handler):
+    __stringer : Stringer
+        An instance of a stringer to track the string status
+
+    Methods
+    _______
+    set_stringer() : void
+        Sets the stringer instance
+
+    process(string) : string[*]
+        Takes input, processes it, and returns output
+
+    __ping() : string[*]
+        Upon corresponding input, pings expectation handler and returns output
+
+    __motion_detected() : string[*]
+        Upon corresponding input, returns output
+
+    __color_detected() : string[*]
+        Upon corresponding input, returns output
+    """
+
+    def __init__(self, protocol_handler, expectation_handler):
         self.__protocol_handler = protocol_handler
+        self.__expectation_handler = expectation_handler
+        self.__stringer = None
 
     def set_stringer(self, stringer):
         """
@@ -18,41 +44,54 @@ class Processor:
 
         self.__stringer = stringer
 
-    def process(self, msg):
+    def process(self, input):
         """
-        Returns message after processing passed message.
-        @param msg  The msg to be processed
+        Returns output after processing passed input.
+        @param input  The input to be processed
         """
 
-        if msg == b"Ping":
-            return [b"Pong"]
-        elif msg == b"Motion":
-            return self.__motion()
-        elif msg == [b"White"]:
-            return self.__color(0)
-        elif msg == [b"Black"]:
-            return self.__color(1)
+        if input == b"Ping":
+            return self.__ping()
+        elif input == b"Motion Detected":
+            return self.__motion_detected()
+        elif input == b"White Detected":
+            return self.__color_detected(0)
+        elif input == b"Black Detected":
+            return self.__color_detected(1)
         else:
             return [b"Unknown Message"]
 
-    def __motion(self):
-        """Returns command in case of motion."""
+    def __ping(self):
+        """Returns output in case of ping."""
+
+        self.__expectation_handler.ping()
+        expired_outputs = self.__expectation_handler.get_expired_outputs()
+
+        if expired_outputs:
+            return expired_outputs
+        else:
+            return [b"Pong"]
+
+    def __motion_detected(self):
+        """Returns output in case of motion."""
 
         if not self.__stringer.complete():
             # The stringer still needs disks
+            self.__expectation_handler.add(b"Confirm Extend Blocker", [b"Retract Blocker"], 10)
             return [b"Extend Blocker", b"Scan Color"]
         else:
             # The stringer does not need disks
             return [b"Ignore"]
 
-    def __color(self, color):
+    def __color_detected(self, color):
         """
-        Returns command in case of color.
+        Returns output in case of color.
         @param color  The color that was detected.
         """
 
         if not self.__stringer.should_pickup(color):
             # We do not want the color
+            self.__expectation_handler.add(b"Confirm Retract Blocker", [b"Extend Blocker"], 10)
             return [b"Retract Blocker"]
         elif not self.__protocol_handler.can_pickup():
             # We are not allowed to pick up a disk
