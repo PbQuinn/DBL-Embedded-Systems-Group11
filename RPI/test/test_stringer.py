253b97b60d9e24a9ce55d384c8637eeae12da921
daanb <daanboelhouwers@gmail.com>
2021-03-12 16:43:21 +0100

    Adjust test cases for Stringer
    Due to get_iteration being removed, some new functionality / exceptions
    need to be tested for.

--- RPI/test/test_stringer.py
+++ RPI/test/test_stringer.py
@@ -8,62 +8,51 @@ class TestInit(TestCase):
         self.assertRaises(ValueError, Stringer, -1)
 
     def test_get_next_color(self):
-        # pattern consisting of only 1s, no disks stringed:
+        # pattern consisting of only 1, no disks stringed:
         self.stringer = Stringer(1)
         self.assertEqual(self.stringer.get_next_color(), 1)
-        # pattern consisting of only 0s, no disks stringed
+        # pattern consisting of only 0, no disks stringed:
         self.stringer = Stringer(0)
         self.assertEqual(self.stringer.get_next_color(), 0)
-        # pattern of length 3, one complete iteration:
+        # pattern of length 3, stringed completely:
         self.stringer = Stringer(6)
         self.assertEqual(self.stringer.get_next_color(), 0)
         self.stringer.stringed_disks = [0]
         self.assertEqual(self.stringer.get_next_color(), 1)
         self.stringer.stringed_disks = [1, 0]
         self.assertEqual(self.stringer.get_next_color(), 1)
-        self.stringer.stringed_disks = [1, 1, 0]
-        self.assertEqual(self.stringer.get_next_color(), 0)
 
-    def test_string_color(self):
-        # pattern consisting of only 1s, stringing 1:
+    def test_get_next_color_exception(self):
+        self.stringer = Stringer(6)
+        self.stringer.stringed_disks = [1, 1, 0]
+        self.assertRaises(Exception, self.stringer.get_next_color)
+
+    def test_string_disk(self):
+        # pattern consisting of only 1, stringing 1:
         self.stringer = Stringer(1)
         self.is_correct = self.stringer.string_disk(1)
         self.assertTrue(self.is_correct)
         self.assertEqual(self.stringer.stringed_disks, [1])
-        # pattern consisting of only 1s, stringing 0:
+        # pattern consisting of only 1, stringing 0:
+        self.stringer = Stringer(1)
         self.is_correct = self.stringer.string_disk(0)
         self.assertFalse(self.is_correct)
-        self.assertEqual(self.stringer.stringed_disks, [0, 1])
+        self.assertEqual(self.stringer.stringed_disks, [0])
         # pattern already partly stringed:
         self.stringer = Stringer(11)
-        self.stringer.stringed_disks = [1, 1, 1, 0, 1, 1]
+        self.stringer.stringed_disks = [1, 1]
         self.is_correct = self.stringer.string_disk(0)
         self.assertTrue(self.is_correct)
-        self.assertEqual(self.stringer.stringed_disks,
-                         [0, 1, 1, 1, 0, 1, 1])
+        self.assertEqual(self.stringer.stringed_disks, [0, 1, 1])
         self.is_correct = self.stringer.string_disk(0)
         self.assertFalse(self.is_correct)
-        self.assertEqual(self.stringer.stringed_disks,
-                         [0, 0, 1, 1, 1, 0, 1, 1])
+        self.assertEqual(self.stringer.stringed_disks, [0, 0, 1, 1])
 
-    def test_get_iteration(self):
-        # pattern length 1, no disks stringed:
-        self.stringer = Stringer(1)
-        self.assertEqual(self.stringer.get_iteration(), 0)
-        self.stringer = Stringer(0)
-        self.assertEqual(self.stringer.get_iteration(), 0)
-        # pattern length 1, 2 disks stringed:
-        self.stringer = Stringer(1)
-        self.stringer.stringed_disks = [1, 0]
-        self.assertEqual(self.stringer.get_iteration(), 2)
-        # pattern length 3, 7 disks stringed:
-        self.stringer = Stringer(4)
-        self.stringer.stringed_disks = [1, 0, 0, 1, 0, 0, 1]
-        self.assertEqual(self.stringer.get_iteration(), 2)
-        # pattern length 7, 3 disks stringed:
-        self.stringer = Stringer(73)
-        self.stringer.stringed_disks = [1, 0, 0]
-        self.assertEqual(self.stringer.get_iteration(), 0)
+    def test_to_string_exception(self):
+        self.stringer = Stringer(11)
+        self.stringer.stringed_disks = [1, 0, 1, 1]
+        self.assertRaises(Exception, self.stringer.string_disk, 1)
+        self.assertRaises(Exception, self.stringer.string_disk, 0)
 
     def test_to_bin_trivial(self):
         self.stringer = Stringer(1)
@@ -108,9 +97,6 @@ class TestInit(TestCase):
         self.assertFalse(self.stringer.is_complete())
         self.stringer.string_disk(1)
         self.assertTrue(self.stringer.is_complete())
-        # One more disk stringed after completion
-        self.stringer.string_disk(1)
-        self.assertTrue(self.stringer.is_complete())
 
     def test_should_pickup_trivial(self):
         # Empty string, pattern all 1s
@@ -136,8 +122,6 @@ class TestInit(TestCase):
         self.stringer.string_disk(1)
         self.assertTrue(self.stringer.should_pickup(1))
         self.assertFalse(self.stringer.should_pickup(0))
-        self.stringer.string_disk(1)
-        self.assertTrue(self.stringer.should_pickup(1))
-        self.assertFalse(self.stringer.should_pickup(0))
+
 
 
