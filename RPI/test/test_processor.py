c70a3313240e6089fc634c83f0d8ebf8c6acde3c
Richard Farla <kasteelvuur@gmail.com>
2021-03-14 20:17:42 +0100

    Update processor test cases

--- RPI/test/test_processor.py
+++ RPI/test/test_processor.py
@@ -10,8 +10,7 @@ class TestInit(TestCase):
 
     def test_process_ping(self):
         # No expired outputs
-        self.processor = Processor(ProtocolHandler(), ExpectationHandler())
-        self.processor.set_stringer(Stringer(0))
+        self.processor = Processor(Stringer(0), ProtocolHandler(), ExpectationHandler())
         self.processor.process(b"Motion Detected")
         for _ in range(9):
             self.assertEqual(self.processor.process(b"Ping"), [b"Pong"])
@@ -20,22 +19,20 @@ class TestInit(TestCase):
 
     def test_process_motion(self):
         # Uncompleted stringer
-        self.processor = Processor(ProtocolHandler(), ExpectationHandler())
         self.stringer = Stringer(0)
-        self.processor.set_stringer(self.stringer)
+        self.processor = Processor(self.stringer, ProtocolHandler(), ExpectationHandler())
         self.assertEqual(self.processor.process(b"Motion Detected"), [b"Extend Blocker", b"Scan Color"])
         # Completed stringer
-        self.stringer.stringed_disks = [0]
+        self.stringer.stringed_disks = [0] * 8
         self.assertEqual(self.processor.process(b"Motion Detected"), [b"Ignore"])
 
     def test_process_white(self):
         # Unwanted color
-        self.protocol = DummyProtocolHandler(False)
-        self.processor = Processor(self.protocol, ExpectationHandler())
-        self.processor.set_stringer(Stringer(1))
+        self.processor = Processor(Stringer(1), ProtocolHandler(), ExpectationHandler())
         self.assertEqual(self.processor.process(b"White Detected"), [b"Retract Blocker"])
         # No permission from protocol
-        self.processor.set_stringer(Stringer(0))
+        self.protocol = DummyProtocolHandler(False)
+        self.processor = Processor(Stringer(0), self.protocol, ExpectationHandler())
         self.assertEqual(self.processor.process(b"White Detected"), [b"Retract Blocker"])
         # Wanted and permission
         self.protocol.set_allowance(True)
@@ -43,18 +40,17 @@ class TestInit(TestCase):
 
     def test_process_black(self):
         # Unwanted color
-        self.protocol = DummyProtocolHandler(False)
-        self.processor = Processor(self.protocol, ExpectationHandler())
-        self.processor.set_stringer(Stringer(0))
+        self.processor = Processor(Stringer(0), ProtocolHandler(), ExpectationHandler())
         self.assertEqual(self.processor.process(b"Black Detected"), [b"Retract Blocker"])
         # No permission from protocol
-        self.processor.set_stringer(Stringer(1))
+        self.protocol = DummyProtocolHandler(False)
+        self.processor = Processor(Stringer(1), self.protocol, ExpectationHandler())
         self.assertEqual(self.processor.process(b"Black Detected"), [b"Retract Blocker"])
         # Wanted and permission
         self.protocol.set_allowance(True)
         self.assertEqual(self.processor.process(b"Black Detected"), [b"Push", b"Retract Blocker"])
 
     def test_process_unknown(self):
-        self.processor = Processor(ProtocolHandler(), ExpectationHandler())
+        self.processor = Processor(Stringer(69), ProtocolHandler(), ExpectationHandler())
         self.assertEqual(self.processor.process(b"Test"), [b"Unknown Message"])
         self.assertEqual(self.processor.process(b"Gibberish"), [b"Unknown Message"])
