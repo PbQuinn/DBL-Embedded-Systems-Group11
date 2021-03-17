616d3c927258b08d36ef51c37957c11fb7335d6f
Richard Farla <kasteelvuur@gmail.com>
2021-03-17 10:53:43 +0100

    Implement test cases for the protocol handler

--- RPI/test/test_protocol_handler.py
+++ RPI/test/test_protocol_handler.py
@@ -1,4 +1,28 @@
 from unittest import TestCase
+from requests import HTTPError
 from RPI.src.protocol_handler import ProtocolHandler
 
-# TODO add test cases for ProtocolHandler
+
+class TestInit(TestCase):
+
+    def test_constructor_exception(self):
+        self.assertRaises(HTTPError, ProtocolHandler, "group11", "x")
+        self.assertRaises(HTTPError, ProtocolHandler, "x", "0G1EH2HF28")
+        self.assertRaises(HTTPError, ProtocolHandler, "x", "x")
+
+    def test_can_pickup(self):
+        self.protocol = ProtocolHandler()
+        self.assertIsInstance(self.protocol.can_pickup(), bool)
+
+    def test_inform_pickup(self):
+        self.protocol = ProtocolHandler()
+        self.assertIsInstance(self.protocol.inform_pickup(), bool)
+
+    def test_inform_color(self):
+        self.protocol = ProtocolHandler()
+        self.protocol.inform_color(0)  # Black - Might want to flip this since it's different for us!?
+        self.protocol.inform_color(1)  # White - Might want to flip this since it's different for us!?
+
+    def test_inform_alive(self):
+        self.protocol = ProtocolHandler()
+        self.protocol.inform_alive()
