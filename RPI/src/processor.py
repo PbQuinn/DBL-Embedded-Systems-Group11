002432be306d4837dca637ef9f3632a564811612
daanb <daanboelhouwers@gmail.com>
2021-03-18 16:22:42 +0100

    Add a reply to secondary color in process
    Adds secondary color as input option well as a function returning an answer
    in case of secondary color.
    
    Furthermore the process function now is one big try - except block.
    The expectation handler should be made to throw an exception when an
    non-present expectation is being removed instead of printing to the console.

--- RPI/src/processor.py
+++ RPI/src/processor.py
@@ -39,35 +39,43 @@ class Processor:
         @param input_  The input to be processed
         """
 
-        # Main interactions:
-        if input_ == b"Ping":
-            return self.__ping()
-        elif input_ == b"Motion Detected":
-            return self.__motion_detected()
-        elif input_ == b"White Detected":
-            return self.__color_detected(0)
-        elif input_ == b"Black Detected":
-            return self.__color_detected(1)
-        elif input_ == b"Confirm Blocker Extended":
-            return self.__blocker_extended()
-        elif input_ == b"Confirm Blocker Retracted":
-            return self.__blocker_retracted()
-        elif input_ == b"Confirm Pusher Push":
-            return self.__pusher_pushed()
-        elif input_ == b"Confirm Stringer Push":
-            return self.__disk_stringed()
+        try:
+            # Main interactions:
+            if input_ == b"Ping":
+                return self.__ping()
+            elif input_ == b"Motion Detected":
+                return self.__motion_detected()
+            elif input_ == b"Primary White":
+                return self.__primary_color_detected(0)
+            elif input_ == b"Primary Black":
+                return self.__primary_color_detected(1)
+            elif input_ == b"Secondary White":
+                return self.__secondary_color_detected(0)
+            elif input_ == b"Secondary Black":
+                return self.__secondary_color_detected(1)
+            elif input_ == b"Confirm Blocker Extended":
+                return self.__blocker_extended()
+            elif input_ == b"Confirm Blocker Retracted":
+                return self.__blocker_retracted()
+            elif input_ == b"Confirm Pusher Push":
+                return self.__pusher_pushed()
+            elif input_ == b"Confirm Stringer Push":
+                return self.__disk_stringed()
 
-        # Startup interactions:
-        elif input_ == b"White Set":
-            pass    # TODO implement
-        elif input_ == b"Black Set":
-            pass    # TODO implement
+            # Startup interactions:
+            elif input_ == b"White Set":
+                pass    # TODO implement
+            elif input_ == b"Black Set":
+                pass    # TODO implement
 
-        # Error interactions:
-        elif input_ == b"Error Occurred":
-            pass    # TODO implement
-        else:
-            return [b"Unknown Message"]
+            # Error interactions:
+            elif input_ == b"Error Occurred":
+                pass    # TODO implement
+            else:
+                return [b"Unknown Message"]
+        except Exception:
+            # TODO add error handling code
+            pass
 
     def __ping(self):
         """Returns output in case of ping."""
@@ -82,34 +90,48 @@ class Processor:
         if not self.__stringer.is_complete():
             # The stringer still needs disks
             self.__expectation_handler.add(b"Confirm Blocker Extended",
-                                           [b"Retract Blocker"], 10)
+                                           [b"Retract Blocker"], 10)    # TODO adjust timer
             return [b"Extend Blocker"]
         else:
             # The stringer does not need disks
             return [b"Ignore"]
 
-    def __color_detected(self, color):
+    def __primary_color_detected(self, color):
         """
         Removes expectation and returns output in case of color.
         @param color  The color that was detected.
         """
 
-        self.__expectation_handler.remove(b"Color Detected")
+        self.__expectation_handler.remove(b"Primary Color Detected")
 
         if not self.__stringer.should_pickup(color):
             # We do not want the color
             self.__expectation_handler.add(b"Confirm Blocker Retracted",
-                                           [b"Extend Blocker"], 10)
+                                           [b"Extend Blocker"], 10)     # TODO adjust timer
             return [b"Retract Blocker"]
         elif not self.__protocol_handler.can_pickup():
             # We are not allowed to pick up a disk
             self.__expectation_handler.add(b"Confirm Blocker Retracted",
-                                           [b"Extend Blocker"], 10)
+                                           [b"Extend Blocker"], 10)     # TODO adjust timer
             return [b"Retract Blocker"]
         else:
             # We want the color and we are allowed to pick up a disk
-            self.__expectation_handler.add(b"Confirm Pusher Push", [], 10)
-            return [b"Push"]
+            self.__expectation_handler.add(b"Confirm Pusher Push", [], 10)                          # TODO adjust timer
+            self.__expectation_handler.add(b"Secondary Color Detected " + bytes(color), [], 10)       # TODO adjust timer
+            return [b"Push Pusher"]
+
+    def __secondary_color_detected(self, color):
+        """
+        Removes expectation and returns output in case of secondary color detected.
+        @param color  The color that was detected.
+        """
+
+        # Remove expectation. If secondary color != primary color,
+        # this expectation will not be present and thus an error will be thrown
+        self.__expectation_handler.remove(b"Secondary Color Detected " + bytes(color))
+        # No error thrown, so string disk
+        self.__expectation_handler.remove(b"Confirm Stringer Push", [], 10)                         # TODO adjust timer
+        return [b"Push Stringer"]
 
     def __blocker_extended(self):
         """
@@ -117,7 +139,7 @@ class Processor:
         """
 
         self.__expectation_handler.remove(b"Confirm Blocker Extended")
-        self.__expectation_handler.add(b"Color Detected", [], 10)
+        self.__expectation_handler.add(b"Primary Color Detected", [], 10)   # TODO adjust timer
         return [b"Scan Color"]
 
     def __blocker_retracted(self):
@@ -133,7 +155,8 @@ class Processor:
         """
 
         self.__expectation_handler.remove(b"Confirm Pusher Push")
-        self.__expectation_handler.add(b"Confirm Blocker Retracted", [b"Extend Blocker"])
+        self.__expectation_handler.add(b"Confirm Blocker Retracted", [b"Extend Blocker"], 10)   # TODO adjust timer
+        self.__expectation_handler.add(b"Secondary Color Detected", [], 10)                     # TODO adjust timer
         return [b"Retract Blocker"]
 
     def __disk_stringed(self):
