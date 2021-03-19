15fefb6782d71f4951a98072798dd162c69eab2d
Richard Farla <kasteelvuur@gmail.com>
2021-03-19 15:57:17 +0100

    Change input to input_

---
+++ RPI/test/test_expectation_handler.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from RPI.src.expectation_handler import ExpectationHandler
+
+
+class TestInit(TestCase):
+
+    def test_ping(self):
+        self.assertEqual(True, False)
+
+    def test_add(self):
+        self.assertEqual(True, False)
+
+    def test_remove(self):
+        self.assertEqual(True, False)
+
+    def test_get_expired_outputs(self):
+        self.assertEqual(True, False)
