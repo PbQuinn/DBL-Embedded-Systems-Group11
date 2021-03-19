72cef6061539489e6d9e1c34d63a87c583c7f4e0
Richard Farla <kasteelvuur@gmail.com>
2021-03-19 16:48:29 +0100

    Implement test cases for the expectation handler

--- RPI/test/test_expectation_handler.py
+++ RPI/test/test_expectation_handler.py
@@ -4,14 +4,38 @@ from RPI.src.expectation_handler import ExpectationHandler
 
 class TestInit(TestCase):
 
-    def test_ping(self):
-        self.assertEqual(True, False)
-
     def test_add(self):
-        self.assertEqual(True, False)
+        self.handler = ExpectationHandler()
+        self.handler.add("input", ["output"], 10)
+        self.handler.add("0", ["1", "2", "3"], 4)
+        self.handler.add("empty", [], 8)
 
     def test_remove(self):
-        self.assertEqual(True, False)
+        # Unexpected input
+        self.handler = ExpectationHandler()
+        self.assertRaises(ValueError, self.handler.remove, "input")
+        self.assertRaises(ValueError, self.handler.remove, "unknown")
+        # Expected input
+        self.handler.add("input", ["output"], 10)
+        self.handler.add("empty", [], 5)  # Possible, but not wanted by simulation
+        self.handler.add("multi", ["0", "1", "2"], 3)
+        self.handler.remove("input")
+        self.handler.remove("empty")
+        self.handler.remove("multi")
 
     def test_get_expired_outputs(self):
-        self.assertEqual(True, False)
+        # No expired outputs
+        self.handler = ExpectationHandler()
+        self.assertEqual([], self.handler.get_expired_outputs())
+        self.handler.add("uno", ["one"], 1)
+        self.handler.add("dos", ["two", "twee"], 2)
+        self.handler.add("empty", [], 2)
+        self.handler.add("hola", ["hello"], 2)
+        self.handler.add("remove", ["gone"], 2)
+        self.assertEqual([], self.handler.get_expired_outputs())
+        # Expired outputs
+        self.handler.ping()
+        self.assertEqual(["one"], self.handler.get_expired_outputs())
+        self.handler.remove("remove")
+        self.handler.ping()
+        self.assertEqual(["hello", "two", "twee"], self.handler.get_expired_outputs())
