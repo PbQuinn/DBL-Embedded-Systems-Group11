98e574401b82b2203dac3c224efd7340a05bebc1
Richard Farla <kasteelvuur@gmail.com>
2021-03-22 19:16:16 +0100

    Update expectation handler test cases

--- RPI/test/test_expectation_handler.py
+++ RPI/test/test_expectation_handler.py
@@ -6,9 +6,9 @@ class TestInit(TestCase):
 
     def test_add(self):
         self.handler = ExpectationHandler()
-        self.handler.add("input", ["output"], 10)
-        self.handler.add("0", ["1", "2", "3"], 4)
-        self.handler.add("empty", [], 8)
+        self.handler.add("input", ["output"], 10, "MESSAGE")
+        self.handler.add("0", ["1", "2", "3"], 4, "MESSAGE")
+        self.handler.add("empty", [], 8, "MESSAGE")
 
     def test_remove(self):
         # Unexpected input
@@ -16,9 +16,9 @@ class TestInit(TestCase):
         self.assertRaises(ValueError, self.handler.remove, "input")
         self.assertRaises(ValueError, self.handler.remove, "unknown")
         # Expected input
-        self.handler.add("input", ["output"], 10)
-        self.handler.add("empty", [], 5)  # Possible, but not wanted by simulation
-        self.handler.add("multi", ["0", "1", "2"], 3)
+        self.handler.add("input", ["output"], 10, "MESSAGE")
+        self.handler.add("empty", [], 5, "MESSAGE")  # Possible, but not wanted by simulation
+        self.handler.add("multi", ["0", "1", "2"], 3, "MESSAGE")
         self.handler.remove("input")
         self.handler.remove("empty")
         self.handler.remove("multi")
@@ -27,11 +27,11 @@ class TestInit(TestCase):
         # No expired outputs
         self.handler = ExpectationHandler()
         self.assertEqual([], self.handler.get_expired_outputs())
-        self.handler.add("uno", ["one"], 1)
-        self.handler.add("dos", ["two", "twee"], 2)
-        self.handler.add("empty", [], 2)
-        self.handler.add("hola", ["hello"], 2)
-        self.handler.add("remove", ["gone"], 2)
+        self.handler.add("uno", ["one"], 1, "MESSAGE")
+        self.handler.add("dos", ["two", "twee"], 2, "MESSAGE")
+        self.handler.add("empty", [], 2, "MESSAGE")
+        self.handler.add("hola", ["hello"], 2, "MESSAGE")
+        self.handler.add("remove", ["gone"], 2, "MESSAGE")
         self.assertEqual([], self.handler.get_expired_outputs())
         # Expired outputs
         self.handler.ping()
